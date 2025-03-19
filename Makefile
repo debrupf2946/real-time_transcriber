34081e8d1c5adeb270d7f27798c942e34db29a23
Mohit Srivastava <srivastavamohit91@gmail.com>
2025-03-19 11:24:32 +0530

    Added more changes

---
+++ Makefile
@@ -0,0 +1,24 @@
+GIT_ROOT := $(shell git rev-parse --show-toplevel)
+INFRA_ROOT := ${GIT_ROOT}/infra
+VERSION ?= local
+RELEASE_VERSION ?= latest
+TF_INFRA_ROOT := ${INFRA_ROOT}
+ECR_REPO_BASE_URL=626635410672.dkr.ecr.us-east-1.amazonaws.com
+WHISPER_RAY_SERVICE_REPO_URL=${ECR_REPO_BASE_URL}/whisper-ray-service
+ 
+ 
+ecr-login:
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REPO_BASE_URL}
+ 
+build-ray-service:
+    docker build --platform linux/amd64 --no-cache -t whisper-ray-service:${RELEASE_VERSION} .
+ 
+push-docker-backend-service: ecr-login
+    docker tag whisper-ray-service:${RELEASE_VERSION} ${WHISPER_RAY_SERVICE_REPO_URL}:${RELEASE_VERSION}
+    docker push ${WHISPER_RAY_SERVICE_REPO_URL}:${RELEASE_VERSION}
+ 
+ 
+build-push-ray-service: build-ray-service push-docker-backend-service
+ 
+deploy-ray-service:
+    kubectl apply -f ${GIT_ROOT}/Whisper-RayService.yaml
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,86 +1,86 @@
-#---------------------------------------------------------------
-# VPC
-#---------------------------------------------------------------
+# #---------------------------------------------------------------
+# # VPC
+# #---------------------------------------------------------------
 
-# WARNING: This VPC module includes the creation of an Internet Gateway and NAT Gateway, which simplifies cluster deployment and testing, primarily intended for sandbox accounts.
-# IMPORTANT: For preprod and prod use cases, it is crucial to consult with your security team and AWS architects to design a private infrastructure solution that aligns with your security requirements
+# # WARNING: This VPC module includes the creation of an Internet Gateway and NAT Gateway, which simplifies cluster deployment and testing, primarily intended for sandbox accounts.
+# # IMPORTANT: For preprod and prod use cases, it is crucial to consult with your security team and AWS architects to design a private infrastructure solution that aligns with your security requirements
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.10"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "~> 5.10"
 
-  name = local.name
-  cidr = local.vpc_cidr
+#   name = local.name
+#   cidr = local.vpc_cidr
 
-  secondary_cidr_blocks = [local.secondary_vpc_cidr]
+#   secondary_cidr_blocks = [local.secondary_vpc_cidr]
 
-  azs = local.azs
-  private_subnets = concat(
-    [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)],
-    [for k, v in local.azs : cidrsubnet(local.secondary_vpc_cidr, 4, k)]
-  )
-  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  # Control Plane Subnets
-  # intra_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+#   azs = local.azs
+#   private_subnets = concat(
+#     [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)],
+#     [for k, v in local.azs : cidrsubnet(local.secondary_vpc_cidr, 4, k)]
+#   )
+#   public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+#   # Control Plane Subnets
+#   # intra_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
 
-  enable_flow_log                      = true
-  create_flow_log_cloudwatch_iam_role  = true
-  create_flow_log_cloudwatch_log_group = true
+#   enable_flow_log                      = true
+#   create_flow_log_cloudwatch_iam_role  = true
+#   create_flow_log_cloudwatch_log_group = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+#   public_subnet_tags = {
+#     "kubernetes.io/role/elb" = 1
+#   }
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-    "karpenter.sh/discovery"          = local.name
-  }
+#   private_subnet_tags = {
+#     "kubernetes.io/role/internal-elb" = 1
+#     "karpenter.sh/discovery"          = local.name
+#   }
 
-  tags = merge(local.tags, {
-    "karpenter.sh/discovery" = local.name
-  })
+#   tags = merge(local.tags, {
+#     "karpenter.sh/discovery" = local.name
+#   })
 
-}
+# }
 
-module "vpc_endpoints" {
-  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "~> 5.0"
+# module "vpc_endpoints" {
+#   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+#   version = "~> 5.0"
 
-  create = true
+#   create = true
 
-  vpc_id = module.vpc.vpc_id
+#   vpc_id = module.vpc.vpc_id
 
 
 
-  endpoints = {
-    s3 = {
-      service         = "s3"
-      service_type    = "Gateway"
-      route_table_ids = module.vpc.private_route_table_ids
-      tags = {
-        Name = "${local.name}-s3"
-      }
-    }
-    ecr_api = {
-      service             = "ecr.api"
-      service_type        = "Interface"
-      subnet_ids          = [module.vpc.private_subnets[0]]
-      private_dns_enabled = true
-      tags = {
-        Name = "${local.name}-ecr-api"
-      }
-    }
-    ecr_dkr = {
-      service             = "ecr.dkr"
-      service_type        = "Interface"
-      subnet_ids          = [module.vpc.private_subnets[0]]
-      private_dns_enabled = true
-      tags = {
-        Name = "${local.name}-ecr-dkr"
-      }
-    }
-  }
-}
+#   endpoints = {
+#     s3 = {
+#       service         = "s3"
+#       service_type    = "Gateway"
+#       route_table_ids = module.vpc.private_route_table_ids
+#       tags = {
+#         Name = "${local.name}-s3"
+#       }
+#     }
+#     ecr_api = {
+#       service             = "ecr.api"
+#       service_type        = "Interface"
+#       subnet_ids          = [module.vpc.private_subnets[0]]
+#       private_dns_enabled = true
+#       tags = {
+#         Name = "${local.name}-ecr-api"
+#       }
+#     }
+#     ecr_dkr = {
+#       service             = "ecr.dkr"
+#       service_type        = "Interface"
+#       subnet_ids          = [module.vpc.private_subnets[0]]
+#       private_dns_enabled = true
+#       tags = {
+#         Name = "${local.name}-ecr-dkr"
+#       }
+#     }
+#   }
+# }
